43d70f341d9a740cc586d51507c77544cfcac5eb
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2020-01-26 01:59:00 -0500

    Head recursion edits

--- Recursion/Head_recursion/HeadRecursion.cpp
+++ Recursion/Head_recursion/HeadRecursion.cpp
@@ -1,16 +1,24 @@
+/*
+	Example of Head recursion in C++
+	Author: Neftali Samarey
+*/
+
 #include <iostream>
 
 using namespace std;
 
+// Prototype function
 void headRecursion(int n);
 
 int main() {
-	// Example of head recursion
+	// Calling recursive function in main
 	headRecursion(8);
 }
 
+// Implemented method
 void headRecursion(int n) {
 	if (n > 0) {
+		// If other statements here, this is not head recursion
 		headRecursion(n - 1);
 		// Call other things after returning
 		cout << n << endl;
